42178d86d1c65061521d52869abdc29233a34bc7
Dinesh Silwal <dineshsilwal855@gmail.com>
2020-05-11 21:25:49 +0545

    Updates

---
+++ precache-manifest.eb5f8ec0816b046359801d49a2492155.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef446dcd6d7710ee46b38caf0015fb5",
+    "url": "/mareeWeb/index.html"
+  },
+  {
+    "revision": "c910a6f192284c68a578",
+    "url": "/mareeWeb/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2a42ba086c1f028be5b9",
+    "url": "/mareeWeb/static/css/main.dbe64193.chunk.css"
+  },
+  {
+    "revision": "c910a6f192284c68a578",
+    "url": "/mareeWeb/static/js/2.d716c00a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mareeWeb/static/js/2.d716c00a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a42ba086c1f028be5b9",
+    "url": "/mareeWeb/static/js/main.2866e45c.chunk.js"
+  },
+  {
+    "revision": "adb542426b86914239c2",
+    "url": "/mareeWeb/static/js/runtime-main.958abf8c.js"
+  },
+  {
+    "revision": "a6749496ee90671b65c6bb96c3a53cb8",
+    "url": "/mareeWeb/static/media/hamburger.a6749496.svg"
+  }
+]);
